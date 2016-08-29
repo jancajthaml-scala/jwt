ac6dcd2272801fba15ed450d30a4bdea700da98c
Jan Cajthaml <jan.cajthaml@gmail.com>
2016-08-29 19:08:16 +0200

    decoding skeleton for plain JWT

--- build.sbt
+++ build.sbt
@@ -5,3 +5,8 @@ version := "0.1.0-SNAPSHOT"
 description := "JSON Web Token issuer and validator"
 
 scalaVersion := "2.11.8"
+
+lazy val demo = (project in file(".")).settings(
+  name := "JWT test",
+  mainClass in Compile := Some("com.github.jancajthaml.jwt.Main")
+)
--- src/main/scala/com/github/jancajthaml/jwt/JWT.scala
+++ src/main/scala/com/github/jancajthaml/jwt/JWT.scala
@@ -1,1 +1,45 @@
 package com.github.jancajthaml.jwt
+
+import java.util.Base64
+
+//@todo remove these dependencies and implement from scratch
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
+
+object Main extends App {
+
+  val encoding: String = "utf-8"
+  val source: String = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ"
+
+  val chunks: Array[String] = source.split('.')
+  //@otod check for length here
+
+  //@todo extract to type/function mayne
+  val header = new String(Base64.getDecoder().decode(chunks(0)))
+
+  //@todo implement vanilla encoding for speed
+  //HS256 -> HmacSHA256
+  val mac: Mac = Mac.getInstance("HmacSHA256")
+  mac.init(new SecretKeySpec("secret".getBytes(encoding), "HmacSHA256"))
+
+  val signature = chunks(2).getBytes(encoding)
+  val calculatedSignature: Array[Byte] = Base64.getEncoder().withoutPadding().encodeToString(mac.doFinal((chunks(0) + ('.' +: chunks(1))).getBytes(encoding))).getBytes(encoding)
+
+  val correct: Boolean = {
+    calculatedSignature.length == signature.length match {
+      case true => (signature zip calculatedSignature).foldLeft (0) {(r, ab) => r + (ab._1 ^ ab._2)} == 0
+      case _ => false
+    }
+  }
+
+  println(s"header: $header")
+
+  //@todo add check
+  println(s"signature ok: $correct")
+
+  //care about payload now
+  val payload = new String(Base64.getDecoder().decode(chunks(1)))
+
+  println(s"payload: $payload")
+
+}
