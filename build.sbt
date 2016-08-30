7a6acae19f33e7ae72adea5cbeffa5058dd17947
Jan Cajthaml <jan.cajthaml@gmail.com>
2016-08-30 12:46:54 +0200

    added skeleton for json handling and multi signature algorithms

--- build.sbt
+++ build.sbt
@@ -1,12 +1,25 @@
+import sbt.Keys._
+
 name := "jwt"
 
-version := "0.1.0-SNAPSHOT"
+version in Global := "0.1.0-SNAPSHOT"
 
 description := "JSON Web Token issuer and validator"
 
-scalaVersion := "2.11.8"
+scalaVersion in Global := "2.11.8"
 
-lazy val demo = (project in file(".")).settings(
+scalacOptions in Global ++= Seq(
+  "-encoding", "UTF-8",
+  "-unchecked",
+  "-deprecation",
+  "-feature",
+  "-Xfatal-warnings",
+  "-Xlint",
+  "-Yrangepos",
+  "-language:postfixOps")
+
+lazy val jwt = project.in(file(".")).settings(
   name := "JWT test",
+  libraryDependencies += "io.spray" %%  "spray-json" % "1.3.2",
   mainClass in Compile := Some("com.github.jancajthaml.jwt.Main")
 )
--- src/main/scala/com/github/jancajthaml/jwt/JWT.scala
+++ src/main/scala/com/github/jancajthaml/jwt/JWT.scala
@@ -1,10 +1,33 @@
 package com.github.jancajthaml.jwt
 
 import java.util.Base64
+import spray.json._ //spray.json.JsValue
+import DefaultJsonProtocol._
 
 //@todo remove these dependencies and implement from scratch
 import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
+
+class Header(val alg: String, val typ: String)
+
+object UniversalJsonProtocol extends DefaultJsonProtocol {
+
+  implicit object HeaderJsonFormat extends RootJsonFormat[Header] {
+    def write(x: Header) = {
+      JsObject((Map[String, Any]() /: x.getClass.getDeclaredFields) {(a, f) =>
+        f.setAccessible(true)
+        a + (f.getName -> f.get(x))
+      } map {case (key, value) => (key.toString -> JsString(value.toString))})
+    }
+
+    def read(value: JsValue) = { value.asJsObject.getFields("alg", "typ") match {
+      case Seq(JsString(alg), JsString(typ)) => new Header(alg, typ)
+      case _ => throw new DeserializationException("Header expected")
+    } }
+  }
+}
+
+import UniversalJsonProtocol._
 
 object Main extends App {
 
@@ -15,12 +38,22 @@ object Main extends App {
   //@otod check for length here
 
   //@todo extract to type/function mayne
-  val header = new String(Base64.getDecoder().decode(chunks(0)))
+  val header: Header = new String(Base64.getDecoder().decode(chunks(0))).parseJson.convertTo[Header]
 
-  //@todo implement vanilla encoding for speed
-  //HS256 -> HmacSHA256
-  val mac: Mac = Mac.getInstance("HmacSHA256")
-  mac.init(new SecretKeySpec("secret".getBytes(encoding), "HmacSHA256"))
+  if (header.typ != "JWT") {
+    println("not JSON Web Token... invalid")
+  }
+
+  val mac: Mac = header.alg match {
+    case "HS256" => {
+      //HS256 -> HmacSHA256
+      println("[i] signed with HMAC SHA 256 bit")
+      val x: Mac = Mac.getInstance("HmacSHA256")
+      x.init(new SecretKeySpec("secret".getBytes(encoding), "HmacSHA256"))
+      x
+    }
+    case x => throw new DeserializationException(s"Unsupported algorithm ${x}")
+  }
 
   val signature = chunks(2).getBytes(encoding)
   val calculatedSignature: Array[Byte] = Base64.getEncoder().withoutPadding().encodeToString(mac.doFinal((chunks(0) + ('.' +: chunks(1))).getBytes(encoding))).getBytes(encoding)
@@ -32,14 +65,18 @@ object Main extends App {
     }
   }
 
-  println(s"header: $header")
+  //println(getCCParams(header))
+
+  //println(s"header: ${header.typ} ${header.alg}")
 
   //@todo add check
   println(s"signature ok: $correct")
 
   //care about payload now
-  val payload = new String(Base64.getDecoder().decode(chunks(1)))
+  val payload = new String(Base64.getDecoder().decode(chunks(1))).parseJson
 
   println(s"payload: $payload")
+
+  println(header.toJson)
 
 }
