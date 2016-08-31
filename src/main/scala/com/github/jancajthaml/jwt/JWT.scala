2eb52ffbac06432303f570d58af1a1aa59f4a3bb
Jan Cajthaml <jan.cajthaml@gmail.com>
2016-08-31 09:24:42 +0200

    implemented JWT encoding and added perf metrics to estimate slowest part of code

--- src/main/scala/com/github/jancajthaml/jwt/JWT.scala
+++ src/main/scala/com/github/jancajthaml/jwt/JWT.scala
@@ -11,6 +11,68 @@ object Main extends App {
 
   val secretKey: String = "secret"
 
+  def dumps(value: Map[String, Any]): String = {
+    "{" + value.map(x => {("\"" + x._1 + "\":\"" + x._2 + "\"")}).mkString("",", ","") + "}"
+  }
+
+  def getAlg(name: Option[Any], key: Array[Byte]): Option[Mac] = {
+    /*val mac: Mac = */
+    name match {
+      case Some("HS256") => { //-> HmacSHA256
+        val x: Mac = Mac.getInstance("HmacSHA256")
+        x.init(new SecretKeySpec(key, "HmacSHA256"))
+        Some(x)
+      }
+      case Some("HS384") => { //-> HmacSHA384
+        val x: Mac = Mac.getInstance("HmacSHA384")
+        x.init(new SecretKeySpec(key, "HmacSHA384"))
+        Some(x)
+      }
+      case Some("HS512") => { //-> HmacSHA512
+        val x: Mac = Mac.getInstance("HmacSHA512")
+        x.init(new SecretKeySpec(key, "HmacSHA512"))
+        Some(x)
+      }
+      case Some("RS256") => { //-> SHA256withRSA
+        //not available by default
+        val x: Mac = Mac.getInstance("SHA256withRSA")
+        x.init(new SecretKeySpec(key, "SHA256withRSA"))
+        Some(x)
+      }
+      case Some("RS384") => { //-> SHA384withRSA
+        //not available by default
+        val x: Mac = Mac.getInstance("SHA384withRSA")
+        x.init(new SecretKeySpec(key, "SHA384withRSA"))
+        Some(x)
+      }
+      case Some("RS512") => { //-> SHA512withRSA
+        //not available by default
+        val x: Mac = Mac.getInstance("SHA512withRSA")
+        x.init(new SecretKeySpec(key, "SHA512withRSA"))
+        Some(x)
+      }
+      case Some("ES256") => { //-> SHA256withECDSA
+        //not available by default
+        val x: Mac = Mac.getInstance("SHA256withECDSA")
+        x.init(new SecretKeySpec(key, "SHA256withECDSA"))
+        Some(x)
+      }
+      case Some("ES384") => { //-> SHA384withECDSA
+        //not available by default
+        val x: Mac = Mac.getInstance("SHA384withECDSA")
+        x.init(new SecretKeySpec(key, "SHA384withECDSA"))
+        Some(x)
+      }
+      case Some("ES512") => { //-> SHA512withECDSA
+        //not available by default
+        val x: Mac = Mac.getInstance("SHA512withECDSA")
+        x.init(new SecretKeySpec(key, "SHA512withECDSA"))
+        Some(x)
+      }
+      case x => None //throw new DeserializationException(s"Unsupported algorithm ${x}")
+    }
+  }
+
   def decode(token: String, encoding: String): Map[String, Any] = {
     val chunks: Array[String] = token.split('.')
     //@todo check for length here
@@ -23,53 +85,9 @@ object Main extends App {
       case x => throw new DeserializationException(s"Invalid type for JWT decoding ${x}")
     }
 
-    val mac: Mac = header.get("alg") match {
-      case Some("HS256") => { //-> HmacSHA256
-        val x: Mac = Mac.getInstance("HmacSHA256")
-        x.init(new SecretKeySpec(secretKey.getBytes(encoding), "HmacSHA256"))
-        x
-      }
-      case Some("HS384") => { //-> HmacSHA384
-        val x: Mac = Mac.getInstance("HmacSHA384")
-        x.init(new SecretKeySpec(secretKey.getBytes(encoding), "HmacSHA384"))
-        x
-      }
-      case Some("HS512") => { //-> HmacSHA512
-        val x: Mac = Mac.getInstance("HmacSHA512")
-        x.init(new SecretKeySpec(secretKey.getBytes(encoding), "HmacSHA512"))
-        x
-      }
-      case Some("RS256") => { //-> SHA256withRSA
-        val x: Mac = Mac.getInstance("SHA256withRSA")
-        x.init(new SecretKeySpec(secretKey.getBytes(encoding), "SHA256withRSA"))
-        x
-      }
-      case Some("RS384") => { //-> SHA384withRSA
-        val x: Mac = Mac.getInstance("SHA384withRSA")
-        x.init(new SecretKeySpec(secretKey.getBytes(encoding), "SHA384withRSA"))
-        x
-      }
-      case Some("RS512") => { //-> SHA512withRSA
-        val x: Mac = Mac.getInstance("SHA512withRSA")
-        x.init(new SecretKeySpec(secretKey.getBytes(encoding), "SHA512withRSA"))
-        x
-      }
-      case Some("ES256") => { //-> SHA256withECDSA
-        val x: Mac = Mac.getInstance("SHA256withECDSA")
-        x.init(new SecretKeySpec(secretKey.getBytes(encoding), "SHA256withECDSA"))
-        x
-      }
-      case Some("ES384") => { //-> SHA384withECDSA
-        val x: Mac = Mac.getInstance("SHA384withECDSA")
-        x.init(new SecretKeySpec(secretKey.getBytes(encoding), "SHA384withECDSA"))
-        x
-      }
-      case Some("ES512") => { //-> SHA512withECDSA
-        val x: Mac = Mac.getInstance("SHA512withECDSA")
-        x.init(new SecretKeySpec(secretKey.getBytes(encoding), "SHA512withECDSA"))
-        x
-      }
-      case x => throw new DeserializationException(s"Unsupported algorithm ${x}")
+    val mac: Mac = getAlg(header.get("alg"), secretKey.getBytes(encoding)) match {
+      case None => throw new DeserializationException(s"Unsupported algorithm")
+      case Some(x) => x
     }
 
     val signature = chunks(2).getBytes(encoding)
@@ -77,6 +95,7 @@ object Main extends App {
       mac.doFinal((chunks(0) + ('.' +: chunks(1))).getBytes(encoding))
     ).getBytes(encoding)
 
+    //fast
     (calculatedSignature.length == signature.length match {
       case true => if ((signature zip calculatedSignature).foldLeft (0) {(r, ab) => r + (ab._1 ^ ab._2)} == 0) true
       case _ => new DeserializationException(s"Invalid token, signature does not match")
@@ -85,70 +104,22 @@ object Main extends App {
     loads(new String(Base64.getDecoder().decode(chunks(1))))
   }
 
-  def encode(payload: Map[String, String], alg: String, encoding: String) = { //: String = {
-
+  def encode(payload: Map[String, Any], alg: String, encoding: String) = {
     val header: Map[String, String] = Map("typ" -> "JWT", "alg" -> alg)
 
-    val mac: Mac = Option(alg) match {
-      case Some("HS256") => { //-> HmacSHA256
-        val x: Mac = Mac.getInstance("HmacSHA256")
-        x.init(new SecretKeySpec(secretKey.getBytes(encoding), "HmacSHA256"))
-        x
-      }
-      case Some("HS384") => { //-> HmacSHA384
-        val x: Mac = Mac.getInstance("HmacSHA384")
-        x.init(new SecretKeySpec(secretKey.getBytes(encoding), "HmacSHA384"))
-        x
-      }
-      case Some("HS512") => { //-> HmacSHA512
-        val x: Mac = Mac.getInstance("HmacSHA512")
-        x.init(new SecretKeySpec(secretKey.getBytes(encoding), "HmacSHA512"))
-        x
-      }
-      case Some("RS256") => { //-> SHA256withRSA
-        val x: Mac = Mac.getInstance("SHA256withRSA")
-        x.init(new SecretKeySpec(secretKey.getBytes(encoding), "SHA256withRSA"))
-        x
-      }
-      case Some("RS384") => { //-> SHA384withRSA
-        val x: Mac = Mac.getInstance("SHA384withRSA")
-        x.init(new SecretKeySpec(secretKey.getBytes(encoding), "SHA384withRSA"))
-        x
-      }
-      case Some("RS512") => { //-> SHA512withRSA
-        val x: Mac = Mac.getInstance("SHA512withRSA")
-        x.init(new SecretKeySpec(secretKey.getBytes(encoding), "SHA512withRSA"))
-        x
-      }
-      case Some("ES256") => { //-> SHA256withECDSA
-        val x: Mac = Mac.getInstance("SHA256withECDSA")
-        x.init(new SecretKeySpec(secretKey.getBytes(encoding), "SHA256withECDSA"))
-        x
-      }
-      case Some("ES384") => { //-> SHA384withECDSA
-        val x: Mac = Mac.getInstance("SHA384withECDSA")
-        x.init(new SecretKeySpec(secretKey.getBytes(encoding), "SHA384withECDSA"))
-        x
-      }
-      case Some("ES512") => { //-> SHA512withECDSA
-        val x: Mac = Mac.getInstance("SHA512withECDSA")
-        x.init(new SecretKeySpec(secretKey.getBytes(encoding), "SHA512withECDSA"))
-        x
-      }
-      case x => throw new DeserializationException(s"Unsupported algorithm ${x}")
+    val headerFinal: String = new String(Base64.getEncoder().withoutPadding().encodeToString(dumps(header).getBytes(encoding)))
+    val payloadFinal: String = new String(Base64.getEncoder().withoutPadding().encodeToString(dumps(payload).getBytes(encoding)))
+
+    val mac: Mac = getAlg(Option(alg), secretKey.getBytes(encoding)) match {
+      case None => throw new DeserializationException(s"Unsupported algorithm")
+      case Some(x) => x
     }
 
-    /*
-    val signature: Array[Byte] = Base64.getEncoder().withoutPadding().encodeToString(
-      mac.doFinal((chunks(0) + ('.' +: chunks(1))).getBytes(encoding))
-    )*/
+    val signature: String = new String(Base64.getEncoder().withoutPadding().encodeToString(
+      mac.doFinal((headerFinal + ('.' +: payloadFinal)).getBytes(encoding))
+    ))
 
-    println(header)
-    println(payload)
-
-    //println(signature)
-
-    //loads(new String(Base64.getDecoder().decode(chunks(1))))
+    s"$headerFinal.$payloadFinal.$signature"
   }
 
   def time[A](a: => A, n:Int) = {
@@ -163,22 +134,39 @@ object Main extends App {
     result
   }
 
-  //println()
+  val sampleJWT: String = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ"
+  val sampleJSON: String = dumps(Map("x" -> "y"))
 
-  encode(Map("foo" -> "bar"), "HS256", "utf-8")
-
-  /*
-  println(decode(
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ",
-    "utf-8"
-  ))
-
+  println("[i] encoding performance")
   var a = 0
-  for (a <- 1 to 10) {
-    time({decode(
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ",
-      "utf-8"
-    )}, 1000)
-  }*/
+  for (a <- 1 to 5) {
+    time({
+      encode(Map("foo" -> "bar", "XXX" -> "XXX"), "HS512", "utf-8")
+    }, 1000)
+  }
+
+  println("[i] decoding performance")
+  var b = 0
+  for (b <- 1 to 5) {
+    time({
+      decode(sampleJWT, "utf-8")
+    }, 1000)
+  }
+
+  println("[i] dumps performance")
+  var c = 0
+  for (c <- 1 to 5) {
+    time({
+      dumps(Map("x" -> "y"))
+    }, 1000)
+  }
+
+  println("[i] loads performance")
+  var d = 0
+  for (d <- 1 to 5) {
+    time({
+      loads(sampleJSON)
+    }, 1000)
+  }
 
 }
